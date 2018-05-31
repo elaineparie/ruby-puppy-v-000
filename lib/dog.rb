4cdeb2ea2090e72b04cc39daa97334e43d12ccec
Elaine Anderson <eparie@bu.edu>
2018-05-31 21:50:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,12 +7,13 @@ attr_accessor :name
 
 def initialize(name)
   @name = name
-  @@all << self 
+  @@all << self
 end
 
-def self.all 
+def self.all
   @@all.each do |song|
     puts song.name
   end 
+end 
 
 end
